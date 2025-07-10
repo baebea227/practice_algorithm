f5312caab598b0be9fa7db33f1bd7abf518aac54
baebea227 <baebea227@gmail.com>
2025-07-10 12:59:52 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <map>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n, m;
+    cin >> n >> m;
+    map<int, int> shortcuts;
+
+    for(int i=0; i<n+m; i++) {
+        int a, b;
+        cin >> a >> b;
+        shortcuts[a] = b;
+    }
+
+    queue<int> q;
+    bool visited[101] = {false};
+    q.push(1);
+    visited[1] = true;
+    int cnt = 0;
+    while(!q.empty()) {
+        int qSize = q.size();
+        for(int i=0; i<qSize; i++) {
+            int qFront = q.front();
+            for(int i=1; i<=6; i++) {
+                int next = qFront + i;
+                if(shortcuts.find(next) != shortcuts.end()) {
+                    next = shortcuts[next];
+                }
+                if(next == 100) {
+                    cout << cnt+1 << '\n';
+                    return 0;
+                }
+                if(!visited[next]) {
+                    q.push(next);
+                    visited[next] = true;
+                }
+            }
+            q.pop();
+        }
+        cnt++;
+    }
+}
