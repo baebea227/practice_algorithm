c0a62d2fd041151eaea67e2eb955b62ed29dc966
baebea227 <baebea227@gmail.com>
2025-07-16 14:31:57 +0900

    [Silver III] Title: Four Squares, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.cc
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
@@ -8,12 +9,17 @@ int main() {
     int n;
     cin >> n;
 
+    vector<int> v;
     for(int i=0; i*i<=n; i++) {
-        for(int j=0; j*j<=n; j++) {
-            for(int k=0; k*k<=n; k++) {
-                for(int l=0; l*l<=n; l++) {
-                    if(i*i+j*j+k*k+l*l == n) {
-                        cout << (i != 0) + (j != 0) + (k != 0) + (l != 0)<< '\n';
+        v.push_back(i*i);
+    }
+
+    for(int i : v) {
+        for(int j : v) {
+            for(int k : v) {
+                for(int l : v) {
+                    if(i+j+k+l == n) {
+                        cout << (i != 0) + (j != 0) + (k != 0) + (l != 0) << '\n';
                         return 0;
                     }
                 }
