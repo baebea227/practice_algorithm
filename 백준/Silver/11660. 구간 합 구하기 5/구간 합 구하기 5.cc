aa0a8e0aa49bbd9fdfcd84bc22006780666eab8c
baebea227 <baebea227@gmail.com>
2025-09-01 16:34:22 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 116 ms, Memory: 10356 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -9,17 +9,10 @@ int main() {
     int N, M;
     cin >> N >> M;
 
-    vector<vector<int>> board(N, vector<int>(N));
-    for(int i=0; i<N; i++) {
-        for (int j=0; j<N; j++) {
+    vector<vector<int>> board(N+1, vector<int>(N+1));
+    for(int i=1; i<=N; i++) {
+        for (int j=1; j<=N; j++) {
             cin >> board[i][j];
-        }
-    }
-
-    vector<vector<int>> ranges(M, vector<int>(4));
-    for(int i=0; i<M; i++) {
-        for(int j=0; j<4; j++) {
-            cin >> ranges[i][j];
         }
     }
 
@@ -27,13 +20,14 @@ int main() {
     vector<vector<int>> prefix(N+1, vector<int>(N+1));
     for(int i=1; i<=N; i++) {
         for(int j=1; j<=N; j++) {
-            prefix[i][j] = board[i-1][j-1] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1];
+            prefix[i][j] = board[i][j] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1];
         }
     }
 
     // 누적합을 이용한 계산
-    for(vector<int> range : ranges) {
-        int x1 = range[0], x2 = range[2], y1 = range[1], y2 = range[3];
+    for(int i=0; i<M; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
         cout << prefix[x2][y2] - prefix[x1-1][y2] - prefix[x2][y1-1] + prefix[x1-1][y1-1] << '\n';
     }
 }
