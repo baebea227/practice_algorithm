f03c3a5e734d64123045b2625702fa72f5948212
baebea227 <baebea227@gmail.com>
2025-09-02 17:16:25 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 72 ms, Memory: 6836 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -2,20 +2,19 @@
 #include <vector>
 using namespace std;
 
-vector<vector<char>> board;
+vector<string> board;
 
-void star(int n, int x, int y) {
-    for(int i=n/3; i<n*2/3; i++) {
-        for(int j=n/3; j<n*2/3; j++) {
-            board[x+i][y+j] = ' ';
-        }
-    }
-    if(n == 3) {
+void draw(int n, int x, int y) {
+    if(n == 1) {
+        board[y][x] = '*';
         return;
     }
-    for(int i=x; i<x+n; i+=n/3) {
-        for(int j=y; j<y+n; j+=n/3) {
-            star(n/3, i, j);
+
+    int div = n/3;
+    for(int dx=0; dx<3; dx++) {
+        for(int dy=0; dy<3; dy++) {
+            if(dx==1 && dy==1) continue;
+            draw(div, x + dx*div, y + dy*div);
         }
     }
 }
@@ -27,13 +26,10 @@ int main() {
     int N;
     cin >> N;
 
-    board.resize(N, vector<char>(N, '*'));
-    star(N, 0, 0);
+    board.assign(N, string(N, ' '));
+    draw(N, 0, 0);
 
-    for(vector<char> row : board) {
-        for(char col : row) {
-            cout << col;
-        }
-        cout << '\n';
+    for(string row : board) {
+        cout << row << '\n';   
     }
 }
